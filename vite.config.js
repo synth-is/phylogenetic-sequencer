a250167c5a5ff74f89dbefe1b3e17721a88b00f2
Björn Þór Jónsson <bangsi@bthj.is>
2025-02-26 23:47:58 +0100

    Vite plugin to serve secure context (https) for audio worklets (via Elementary)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   base: '/', // Change this if not serving from root
   build: {
     outDir: 'dist',
